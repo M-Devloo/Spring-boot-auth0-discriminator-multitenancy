cbc7cb0857d2bf4c3fcd4de85e2d9604b6ed6024
Michael Devloo <36577596+M-Devloo@users.noreply.github.com>
2020-09-27 19:04:59 +0200

    Initialization of the project + setting correct dependencies in POM file

---
+++ src/main/java/com/github/mdevloo/springdiscriminatormultitenancyauth0/SpringDiscriminatorMultitenancyAuth0Application.java
@@ -0,0 +1,13 @@
+package com.github.mdevloo.springdiscriminatormultitenancyauth0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDiscriminatorMultitenancyAuth0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDiscriminatorMultitenancyAuth0Application.class, args);
+	}
+
+}
