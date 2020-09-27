d7b5c20ecaaea535ec93e9e2910ee765a651071a
Michael Devloo <36577596+M-Devloo@users.noreply.github.com>
2020-09-28 00:35:57 +0200

    Additional endpoint which returns all the inventory of that particular user (without any inputs). Through the hibernate filter, it will automatically add a where clause with the tenantId which is grabbed from the JWT token

--- src/main/java/com/github/mdevloo/multi/tenancy/core/web/inventory/InventoryController.java
+++ src/main/java/com/github/mdevloo/multi/tenancy/core/web/inventory/InventoryController.java
@@ -3,6 +3,7 @@ package com.github.mdevloo.multi.tenancy.core.web.inventory;
 import com.github.mdevloo.multi.tenancy.core.inventory.domain.Inventory;
 import com.github.mdevloo.multi.tenancy.core.inventory.usecase.CreateNewInventory;
 import com.github.mdevloo.multi.tenancy.core.inventory.usecase.FindInventory;
+import com.github.mdevloo.multi.tenancy.core.inventory.usecase.GetAllInventory;
 import com.github.mdevloo.multi.tenancy.core.inventory.usecase.InventoryCreationRequest;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,7 +17,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.NotNull;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import static com.github.mdevloo.multi.tenancy.core.AppConstants.INVENTORY_URL;
 
@@ -29,11 +32,19 @@ public class InventoryController {
 
   private final CreateNewInventory createNewInventory;
   private final FindInventory findInventory;
+  private final GetAllInventory getAllInventory;
   private final Converter<Inventory, InventoryDTO> inventoryConverter;
 
   @GetMapping
   public InventoryDTO getInventory(@NotNull @RequestParam final UUID id) {
     return this.inventoryConverter.convert(this.findInventory.execute(id));
+  }
+
+  @GetMapping
+  public List<InventoryDTO> getAllInventory() {
+    return this.getAllInventory.execute().stream()
+        .map(this.inventoryConverter::convert)
+        .collect(Collectors.toUnmodifiableList());
   }
 
   @PostMapping
