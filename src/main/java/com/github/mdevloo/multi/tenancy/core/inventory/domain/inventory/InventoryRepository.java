8ded32c9d7bd311d5060cdfe540a94feea585a3c
Michael Devloo <36577596+M-Devloo@users.noreply.github.com>
2020-10-12 22:46:09 +0200

    Completing the integration tests to verify the default JpaRepository & JpaSpecificationExecutor methods to be complaint with the Multi Tenancy implementation. Custom Repository methods like FindAllByName() are automatically using CriteriaBuilder, so these are also compliant. Adding Jpa Model gen for building typesafe Specification functions

--- src/main/java/com/github/mdevloo/multi/tenancy/core/inventory/domain/inventory/InventoryRepository.java
+++ src/main/java/com/github/mdevloo/multi/tenancy/core/inventory/domain/inventory/InventoryRepository.java
@@ -1,7 +1,9 @@
 package com.github.mdevloo.multi.tenancy.core.inventory.domain.inventory;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.UUID;
 
-public interface InventoryRepository extends JpaRepository<Inventory, UUID> {}
+public interface InventoryRepository
+    extends JpaRepository<Inventory, UUID>, JpaSpecificationExecutor<Inventory> {}
--- src/test/java/com/github/mdevloo/multi/tenancy/core/inventory/domain/vendor/VendorRepositoryTest.java
+++ src/test/java/com/github/mdevloo/multi/tenancy/core/inventory/domain/vendor/VendorRepositoryTest.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
